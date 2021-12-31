47f6d4de8cb3ff85b4931461df16110712f50e49
ToXxXika <MontaHorse123@@!!>
2021-12-31 01:27:20 +0100

    NEW PROJECT

--- src/com/company/Classes/Cinema.java
+++
@@ -1,4 +0,0 @@
-package com.company.Classes;
-
-public class Cinema {
-}
--- src/com/company/Interfaces/ISalle.java
+++
@@ -1,9 +0,0 @@
-package com.company.Interfaces;
-
-import com.company.Classes.Salle;
-
-import java.sql.SQLException;
-
-public interface ISalle {
-    Salle RechercherSalle(int idSalle) throws SQLException;
-}
